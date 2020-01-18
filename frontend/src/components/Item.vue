716fde3c891e195282fc55c2fe074a6ba385d956
waterproofpatch <waterproofpatch@gmail.com>
2020-01-18 07:50:31 -0500

    adding item, items can now view an item

---
+++ frontend/src/components/Item.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2 v-if="error">Error: {{error}}</h2>
+    <p>
+      an item
+      <p v-if="itemId">Item {{itemId}}</p>
+    </p>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "Item",
+  props: ['itemId'],
+  data() {
+    return {
+      error: null
+    };
+  },
+  mounted() {
+    if (this.$store.state.uid == null) {
+      this.$router.push("login");
+      return;
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
