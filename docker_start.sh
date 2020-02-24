d666c7c58a2e81ebf917091ed98bf4cb80de3007
waterproofpatch <waterproofpatch@gmail.com>
2020-02-23 20:53:11 -0500

    comments

--- docker_start.sh
+++ docker_start.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 
-echo "Starting UWSGI..."
-
+# Init the database with no test data
+echo "Init database..."
 source /venv/bin/activate && python -m backend.app --init
+
+# Start the WSGI server so the Flask app can receive requests from NGIN
+echo "Starting UWSGI..."
 source /venv/bin/activate && uwsgi --ini /usr/share/wsgi/wsgi.ini &
 
+# Start the NGINX (daemon off so docker container doesn't exit)
 echo "Starting NGINX..."
 exec nginx -g 'daemon off;'
-
-echo "Done."
 
