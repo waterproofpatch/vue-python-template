13a8a9152d23b8b30a82be5db373e786370bffde
waterproofpatch <waterproofpatch@gmail.com>
2020-03-22 19:25:07 -0400

    added a todo and some formatting

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -5,13 +5,26 @@
       <p>test@gmail.com | passwordpassword</p>
       <form class="form form-auth">
         <div class="form-field">
-          <input placeholder="Email" type="text" v-model="email">
+          <input
+            placeholder="Email"
+            type="text"
+            v-model="email"
+          >
         </div>
         <div class="form-field">
-        <input placeholder="Password" type="password" v-model="password">
+          <input
+            placeholder="Password"
+            type="password"
+            v-model="password"
+          >
         </div>
         <div class="form-field">
-        <input class="btn" type="submit" value="Login" v-on:click.prevent="doLogin">
+          <input
+            class="btn"
+            type="submit"
+            value="Login"
+            v-on:click.prevent="doLogin"
+          >
         </div>
       </form>
     </div>
@@ -31,6 +44,7 @@ export default {
       prevRoute: null
     };
   },
+  // TODO move this to main
   beforeRouteEnter(to, from, next) {
     next(vm => {
       console.log("beforeRouterEnter: " + from.path);
@@ -46,16 +60,16 @@ export default {
           password: this.password
         })
         .then(response => {
-          this.$store.commit("login", 
-            {uid: response.data.uid, email: response.data.email}
-          );
+          this.$store.commit("login", {
+            uid: response.data.uid,
+            email: response.data.email
+          });
           this.$router.push(this.prevRoute);
         })
         .catch(error => {
           if (error.response.status == 400) {
             this.error = error.response.data.error;
-          }
-          else {
+          } else {
             this.error = error.response.status;
           }
         })
