203acc6332cce4569d526f3c13a2e61c91ccb654
waterproofpatch <waterproofpatch@gmail.com>
2020-03-26 13:28:29 -0400

    arrowLeft

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,13 +9,16 @@ import Items from "./components/Items.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+library.add(faArrowLeft);
 library.add(faTrash);
 library.add(faPlus);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
+// Vuex store
 import store from "./store";
 
 Vue.config.productionTip = false;
