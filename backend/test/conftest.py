378b2e10c7c44525efc3809d8e50c9a13a3b0576
waterproofpatch <waterproofpatch@gmail.com>
2020-04-05 12:03:27 -0400

    conftest uses flask_app from init

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,19 +1,12 @@
 import pytest
 import tempfile
 
-
-from backend import create_app, create_api, create_db, create_jwt
-# from backend import app
-from backend import views
+from backend import flask_app, app
 
 
 @pytest.fixture
 def client():
-    # get a Flask app
-    the_app = create_app()
-
-    # db_fd, the_app.config['DATABASE'] = tempfile.mkstemp()
-    the_app.config['TESTING'] = True
+    flask_app.config['TESTING'] = True
     print('fixture running')
-    with the_app.test_client() as client:
+    with flask_app.test_client() as client:
         yield client
