73b9c05696bd980210a0fdc638c8fad805a0557c
waterproofpatch <waterproofpatch@gmail.com>
2020-04-09 21:34:17 -0400

    db working

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,12 +1,18 @@
 import pytest
 import tempfile
+import os
 
 from backend import flask_app, app
 
 
 @pytest.fixture
 def client():
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    db_path = os.path.join(basedir, 'test_app.db')
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
     flask_app.config['TESTING'] = True
-    print('fixture running')
+
+    app.init_db(test_data=True, drop_all=True)
+
     with flask_app.test_client() as client:
         yield client
