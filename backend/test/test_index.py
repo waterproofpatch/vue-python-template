d299624c2573053d93ce9c37febe3e02ba1a9aa3
waterproofpatch <waterproofpatch@gmail.com>
2020-04-10 23:07:35 -0400

    sqlalchemy upgrade; more items tests

--- backend/test/test_index.py
+++ backend/test/test_index.py
@@ -1,7 +1,14 @@
 import pytest
 
 
-def test_index(client):
+def test_items(client):
+    """
+    Test the items endpoint
+    """
     res = client.get('/api/items')
-    print(res)
+    assert 200 == res.status_code
+    assert 'application/json' == res.content_type
+    # not expecting any items
+    assert not res.get_json()
+
     assert True
