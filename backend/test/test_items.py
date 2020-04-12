1220eaf8f7016d2d70c6dc0103f2a89f7d3cce9c
waterproofpatch <waterproofpatch@gmail.com>
2020-04-12 11:01:10 -0400

    unauth client test

--- backend/test/test_items.py
+++ backend/test/test_items.py
@@ -7,11 +7,12 @@ from flask_jwt_extended import create_access_token, create_refresh_token
 
 def test_get_items(client):
     """
-    Test the items endpoint
+    Test the items endpoint, retreival
     """
     res = client.get('/api/items')
     assert 200 == res.status_code
     assert 'application/json' == res.content_type
+
     # not expecting any items
     assert not res.get_json()
 
@@ -25,6 +26,7 @@ def test_post_items_fail_empty_payload(authenticated_client):
     res = authenticated_client.post('/api/items', json={})
     assert 400 == res.status_code
     assert 'error' in res.json
+    assert 'missing' in res.json['error']
 
 
 def test_post_items_fail_content_type(authenticated_client):
@@ -34,6 +36,15 @@ def test_post_items_fail_content_type(authenticated_client):
     res = authenticated_client.post('/api/items')
     assert 400 == res.status_code
     assert 'error' in res.json
+    assert 'invalid content type' in res.json['error']
+
+
+def test_post_items_fail_unauthenticated(client):
+    """
+    Test that items endpoint fails when content type is wrong
+    """
+    res = client.post('/api/items')
+    assert 401 == res.status_code
 
 
 def test_post_items_success(authenticated_client):
