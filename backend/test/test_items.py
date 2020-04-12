149fd720117ad9ad4775a3d1b8923c8e9dc2a6b4
waterproofpatch <waterproofpatch@gmail.com>
2020-04-12 10:12:57 -0400

    client auth without testing endpoint

--- backend/test/test_items.py
+++ backend/test/test_items.py
@@ -2,6 +2,8 @@ import json
 
 import pytest
 
+from flask_jwt_extended import create_access_token, create_refresh_token
+
 
 def test_get_items(client):
     """
@@ -24,11 +26,16 @@ def test_post_items(client):
     # we haven't logged in
     assert 401 == res.status_code
 
-    res = client.post(
-        '/api/login',
-        json={'email': 'test@gmail.com', 'password': 'passwordpassword'})
-    assert 200 == res.status_code
-
-    # res = client.post('/api/items')
-    # # we haven't logged in
+    # get some valid access tokens
+    access_token = create_access_token(identity='test@gmail.com')
+    refresh_token = create_refresh_token(identity='test@gmail.com')
+    client.set_cookie('/', 'access_token_cookie', access_token)
+    client.set_cookie('/', 'refresh_token_cookie', refresh_token)
+    # res = client.post(
+    #     '/api/login',
+    #     json={'email': 'test@gmail.com', 'password': 'passwordpassword'})
     # assert 200 == res.status_code
+
+    res = client.post('/api/items')
+    # we haven't logged in
+    assert 200 == res.status_code
