b01e125e71436212347b00ab527ea1127e7dd123
waterproofpatch <waterproofpatch@gmail.com>
2020-04-12 23:32:03 -0400

    commented local sock; removed comment

--- docker_start.sh
+++ docker_start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-# Init the database with no test data
+# Init the database with test data
 echo "Init database..."
-source /venv/bin/activate && python -m backend.app --init
+source /venv/bin/activate && python -m backend.app --init --testdata
 
 # Start the WSGI server so the Flask app can receive requests from NGIN
 echo "Starting UWSGI..."
